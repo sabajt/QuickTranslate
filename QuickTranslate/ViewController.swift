2c5b3dd9a50cbc366fa89d0704438ce459707a20
John Saba <john.thomas.saba@gmail.com>
2016-04-28 19:54:00 -0500

    initial commit

---
+++ QuickTranslate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickTranslate
+//
+//  Created by John Saba on 4/28/16.
+//  Copyright © 2016 John Saba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
