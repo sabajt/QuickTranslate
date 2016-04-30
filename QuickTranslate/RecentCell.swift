d19373efaa2e57256d203d230895cc93ffa76fd1
John Saba <john.thomas.saba@gmail.com>
2016-04-30 12:54:03 -0500

    Begin recent view controller

---
+++ QuickTranslate/RecentCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecentCell.swift
+//  QuickTranslate
+//
+//  Created by John Saba on 4/30/16.
+//  Copyright © 2016 John Saba. All rights reserved.
+//
+
+import UIKit
+
+class RecentCell: UITableViewCell {
+
+}
---
+++ QuickTranslate/RecentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecentViewController.swift
+//  QuickTranslate
+//
+//  Created by John Saba on 4/30/16.
+//  Copyright © 2016 John Saba. All rights reserved.
+//
+
+import UIKit
+
+class RecentViewController: UIViewController {
+
+}
+
+extension RecentViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RecentCell", forIndexPath: indexPath)
+        return cell
+    }
+}
+
+extension RecentViewController: UITableViewDelegate {
+    
+}
