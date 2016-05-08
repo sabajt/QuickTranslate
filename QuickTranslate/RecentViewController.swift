0fae14933c6f1dc2f405898c67d9770ccb823b66
John Saba <john.thomas.saba@gmail.com>
2016-05-08 18:06:05 -0500

    Improve recent translation cells

--- QuickTranslate/RecentViewController.swift
+++ QuickTranslate/RecentViewController.swift
@@ -14,6 +14,7 @@ class RecentViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.separatorStyle = .None
         tableView.estimatedRowHeight = 60
         tableView.rowHeight = UITableViewAutomaticDimension
     }
